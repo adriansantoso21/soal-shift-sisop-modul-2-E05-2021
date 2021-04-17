5e9896136f0363eb490d067537fe1c014929fda3
Oni Ahmad <46712776+oni4hmad@users.noreply.github.com>
2021-04-17 21:23:32 +0700

    Update soal3.c

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -7,49 +7,59 @@
 #include <unistd.h>
 #include <syslog.h>
 #include <string.h>
+#include <wait.h>
+#include <time.h>
 
-int check(char file[])
-{
-    FILE *fp;
+void createKillerExecutable(char *argv, pid_t sid) {
+    FILE *killer_bash = fopen("killer.sh", "w");
 
-    if (fp = fopen(file, "r"))
-    {
-        fclose(fp);
-        return 1;
+    fprintf(killer_bash, "#!/bin/bash\n");
+    if (strcmp(argv, "-z") == 0) {
+        char *kill_code = "killall -9 ./soal3\n";
+        fprintf(killer_bash, kill_code, sid);
+    } else if (strcmp(argv, "-x") == 0) {
+        char *kill_code = "kill %d\n";
+        fprintf(killer_bash, kill_code, getpid());
     }
-    return 0;
+
+    fclose(killer_bash);
 }
 
-int main()
-{
-    pid_t pid, sid; // Variabel untuk menyimpan PID
+void forkExitFailure(pid_t pid) {
+    if (pid < 0) {
+        exit(EXIT_FAILURE);
+    }
+}
 
-    pid = fork();   // Menyimpan PID dari Child Process
+int main(int argc, char *argv[]) {
+    
+    // Return 1 (error) argumen salah
+    if (argc != 2 || (strcmp(argv[1], "-z") != 0 && strcmp(argv[1], "-x") != 0)) {
+        printf("error!\n");
+        return 1;
+    }
+
+    pid_t pid, sid,
+        child_id, child_id2, child_id3, child_id4;   // Variabel untuk menyimpan PID
+
+    pid = fork();     // Menyimpan PID dari Child Process
 
     /* Keluar saat fork gagal
     * (nilai variabel pid < 0) */
-    if (pid < 0)
-    {
+    if (pid < 0) {
         exit(EXIT_FAILURE);
     }
 
     /* Keluar saat fork berhasil
     * (nilai variabel pid adalah PID dari child process) */
-    if (pid > 0)
-    {
+    if (pid > 0) {
         exit(EXIT_SUCCESS);
     }
 
     umask(0);
 
     sid = setsid();
-    if (sid < 0)
-    {
-        exit(EXIT_FAILURE);
-    }
-
-    if ((chdir("/media/sf_soal-shift-sisop-modul-2-E05-2021/soal3")) < 0)
-    {
+    if (sid < 0) {
         exit(EXIT_FAILURE);
     }
 
@@ -57,40 +67,99 @@ int main()
     close(STDOUT_FILENO);
     close(STDERR_FILENO);
 
-    while (1)
-    {
-        int flag;
-        char isi[100];
-        FILE *file;
+    createKillerExecutable(argv[1], sid);
 
-        flag = check("error.txt");
-        if (flag)
-        {
-            file = fopen("error.txt", "r");
-            fgets(isi, 100, file);
-            fclose(file);
+    while (1) {
+        time_t rawtime;
+        time(&rawtime);
+        struct tm *timeinfo  = localtime(&rawtime);
+        char folderName[100];
+        strftime(folderName, 100, "%Y-%m-%d_%H-%M-%S", timeinfo);
+
+        child_id = fork();
+        forkExitFailure(child_id);
+
+        // child 1
+        if (child_id == 0) {
+            char *argv[] = {"mkdir", folderName, NULL};
+            execv("/bin/mkdir", argv);
         }
 
-        int c = 1;
-        char str[15];
-        while (1)
-        {
-            sprintf(str, "error.log.%d", c);
-            if (check(str))
-            {
-                c += 1;
+        // parent 1
+        while (wait(NULL) > 0);
+
+        child_id2 = fork();
+        forkExitFailure(child_id2);
+
+        // child 2
+        if (child_id2 == 0) {
+            for(int i = 0; i < 10; i++) {
+                
+                child_id3 = fork();
+                forkExitFailure(child_id3);
+                
+                // child 3
+                if (child_id3 == 0) {
+                    char pictName[100], pictPath[200], link[100];
+                    
+                    time(&rawtime);
+                    timeinfo = localtime(&rawtime);
+                    strftime(pictName, 100, "%Y-%m-%d_%H-%M-%S", timeinfo);
+
+                    sprintf(pictPath, "%s/%s", folderName, pictName);
+                    sprintf(link, "https://picsum.photos/%ld", (rawtime % 1000) + 50);
+
+                    char *argv[] = {"wget", "-O", pictPath, link, NULL};
+                    execv("/usr/bin/wget", argv);
+                }
+
+                // parent 3
+                sleep(5);
             }
-            else
-                break;
-        }
 
-        file = fopen(str, "w");
-        fputs(isi, file);
-        fclose(file);
+            child_id4 = fork();
+            forkExitFailure(child_id4);
 
-        file = fopen("error.txt", "w");
-        fputs("", file);
+            // child 4
+            if (child_id4 == 0) {
+                char message[100] = "Download Success", zipName[100], txtPath[200];
+                
+                for (int j = 0; j < strlen(message); j++) {
+                    if (message[j] >= 'a' && message[j] <= 'z') {
+                        message[j] = message[j] - 'a';
+                        message[j] = (message[j] + 5) % 26;
+                        message[j] = message[j] + 'a';
+                    }
+
+                    else if (message[j] >= 'A' && message[j] <= 'Z') {
+                        message[j] = message[j] - 'A';
+                        message[j] = (message[j] + 5) % 26;
+                        message[j] = message[j] + 'A';
+                    }
+                }
+
+                sprintf(txtPath, "%s/%s", folderName, "status.txt");
+                
+                FILE *txt = fopen(txtPath, "w");
+                fputs(message, txt);
+                fclose(txt);
+
+                sprintf(zipName, "%s.zip", folderName);
+                
+                char *argv[] = {"zip", "-r", zipName, folderName, NULL};
+                execv("/usr/bin/zip", argv);
+                
+            } else {
+
+                // parent 4
+                while (wait(NULL) > 0);
+            
+                char *argv[] = {"rm", "-r", folderName, NULL};
+                execv("/bin/rm", argv);
+            }
+        } 
         
+        // parent 2
         sleep(40);
     }
 }
